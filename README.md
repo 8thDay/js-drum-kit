2b9522527f55e12728b8232ce02d06b43268ffa3
Antonia Gibbons <toni@tonigibbons.co.uk>
2016-12-15 17:43:10 +0000

    ver 0.1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # js-drum-kit
+## A Drum Kit Project Using Vanilla Javascript
+Based on coding challenge #1 at [javascript30.com](https://javascript30.com)
+### Additional Features
+Responsive design.
+Responds to click events in addition to keydown.
---
+++ js/script.js
@@ -0,0 +1,32 @@
+function playSound(keyCode) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`); // select the audio element with the given keyCode
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`);    // select the element with a class of "key" and the given keyCode
+  if (!audio) { // if there isn't an audio element with given key...
+    return;     // stop the function from running
+  }
+  audio.currentTime = 0; // rewind to start
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') {
+    return; // ignore it if it's not a transform
+  }
+  this.classList.remove('playing');
+}
+
+// wait for click on div.key, then play sound and perform transitions
+const clickedKey = document.querySelectorAll('.key');
+clickedKey.forEach(key => key.addEventListener('click', function(e) {
+  playSound (e.path[1].dataset.key);
+}));
+
+// wait for key to be pressed, then play sound and perform transitions
+window.addEventListener('keydown', function(event) {
+  playSound(event.keyCode);
+});
+
+// wait for transition to end, then remove it
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
